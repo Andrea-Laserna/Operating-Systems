65bf9b19658e596602ef4796b2d3f2ecac153d64
Andrea Laserna <aalaserna@up.edu.ph>
2026-02-04 16:07:13 +0800

    docs: commented on child process execvp

--- main.c
+++ main.c
@@ -2,16 +2,31 @@
 
 int main(int argcount, char **argvect) {
 
+    // Silening warning: not using this parameter yet
     (void)argcount;
+    // Store exit status
     int status;
 
     // LOOP this
 
-    // Child process
+    // Child process: Launch the requested command in a separate process
     if (fork() == 0)
+        /*
+            Search PATH when given command name
+            argvect[0] is shell itself
+            argvect[1] is the command
+            argvect[2] are the arguments
+
+            Replace child with the requested program:
+            argvect + 1 is passed to execvp so the child's argvect[0] becomes argvect[1] 
+
+            If execvp succeeds, child process image is replaced and never reaches wait
+            If execvp fails, child continues and will call wait(&status), which will fail
+            because the child has no children
+        */ 
         execvp(argvect[1], argvect + 1);
     
-    // Parent waits for the child and stalk inside status the return value of the child
+    // Parent waits for the child
     wait(&status);
 
     return EXIT_SUCCESS; 
