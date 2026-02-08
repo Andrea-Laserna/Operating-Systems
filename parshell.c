0aaac856deddd949a87870fdcfb62b8be8f8328f
Coi <ctoyco1@up.edu.ph>
2026-02-09 03:09:20 +0800

    feat: implemented tokenization using strtok

--- parshell.c
+++ parshell.c
@@ -7,27 +7,35 @@ understanding of the concepts tackled in class and learned with youtube tutorial
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
 
 	size_t bufsize = 0;
-	char *buffer = NULL; 							// let getline allocate buffer since we don't know how but the input is anyway
-
+	char *command = NULL; 							// let getline allocate buffer since we don't know how but the input is anyway
+	char *delimiter = " \n";
+	char *token; 
 
 
 	printf("parshell> ");
-	if (getline(&buffer, &bufsize, stdin) == -1) {
+	if (getline(&command, &bufsize, stdin) == -1) {
 		perror("getline"); 							// to know where the error is coming from
-		free(buffer);
+		free(command);								// Free the allocated memory to prevent memory leak
 		return EXIT_FAILURE; 
-
 	}
 
-	printf("You entered: %s", buffer);
-	free(buffer);
+	token = strtok(command, delimiter);				// returns a pointer to a null-terminated string containing the next token.
+	while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, delimiter);
+    }
 
 
-	return 0; 
+	printf("You entered: %s", command);
+	free(command);
+
+
+	return EXIT_SUCCESS; 
 }
 
 
@@ -37,4 +45,7 @@ Design Decisions:
 	- It is a convenient way to read an entire line of input, regardless of its length.
 	- It automatically allocates a buffer and resizes it as needed, which simplifies memory management.
 	- It handles the newline character at the end of the input, which can be useful for processing commands.
+	* why use EXIT_SUCCESS and EXIT_FAILURE?
+		- for future automated testing, it is easier to check for these standard exit codes rather than arbitrary integers.
+
 */
