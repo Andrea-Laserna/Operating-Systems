64ecd0b5b3532c4f36663b8552672d9ea0c1259a
Coi <ctoyco1@up.edu.ph>
2026-02-09 02:24:49 +0800

    docs: add getline Design Decisions

--- parshell.c
+++ parshell.c
@@ -11,10 +11,17 @@ understanding of the concepts tackled in class and learned with youtube tutorial
 int main(void){
 
 	size_t bufsize = 0;
-	char *buffer = NULL; 				// let getline allocate buffer since we don't know how but the input is anyway
+	char *buffer = NULL; 							// let getline allocate buffer since we don't know how but the input is anyway
+
+
 
 	printf("parshell> ");
-	getline(&buffer, &bufsize, stdin);
+	if (getline(&buffer, &bufsize, stdin) == -1) {
+		perror("getline"); 							// to know where the error is coming from
+		free(buffer);
+		return EXIT_FAILURE; 
+
+	}
 
 	printf("You entered: %s", buffer);
 	free(buffer);
@@ -22,3 +29,12 @@ int main(void){
 
 	return 0; 
 }
+
+
+/*
+Design Decisions: 
+1. why use getline?
+	- It is a convenient way to read an entire line of input, regardless of its length.
+	- It automatically allocates a buffer and resizes it as needed, which simplifies memory management.
+	- It handles the newline character at the end of the input, which can be useful for processing commands.
+*/
